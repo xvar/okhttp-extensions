6b5190ced17a2d96523e9797088a65f3857384f7
Alexey Khaiminov <alex.haiminov@gmail.com>
2022-12-26 00:10:07 +0300

    add lib and sample project

---
+++ lib/build.gradle.kts
@@ -0,0 +1,34 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("java")
+    kotlin("jvm") version "1.7.20"
+    `maven-publish`
+}
+
+group = "xvar.okhttp.ext"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly(platform("com.squareup.okhttp3:okhttp-bom:4.10.0"))
+    compileOnly("com.squareup.okhttp3:okhttp")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "Okhttp-Extensions"
+include("lib")
